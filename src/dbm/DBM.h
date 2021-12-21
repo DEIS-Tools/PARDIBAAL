d98e941a78dec85658543ac79e382c0b0f2314bc
Thomas M.G <tgrose17@student.aau.dk>
2021-12-21 15:12:46 +0100

    add copy of LGPL license and copyright/license notices in each file

--- src/dbm/DBM.h
+++ src/dbm/DBM.h
@@ -1,6 +1,24 @@
-//
-// Created by Thomas Grosen on 13/10/2021.
-//
+/*
+ * Copyright Thomas Møller Grosen
+ * Created on 13/10/2021.
+ */
+
+/*
+ * This file is part of dbm2
+ *
+ * dbm2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dbm2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with dbm2.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef BDM_DBM_H
 #define BDM_DBM_H
