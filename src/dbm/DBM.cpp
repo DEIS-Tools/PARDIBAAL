c05264888ebd203daeb36629d77293f289173395
Thomas M.G <tgrose17@student.aau.dk>
2021-12-22 11:29:25 +0100

    replace additional "-1*" with "-"

--- src/dbm/DBM.cpp
+++ src/dbm/DBM.cpp
@@ -95,7 +95,7 @@ namespace dbm {
     void DBM::assign(dim_t x, val_t m) {
         for (dim_t i = 0; i < _bounds_table._number_of_clocks; i++) {
             _bounds_table.get(x, i) = bound_t(m, false) + _bounds_table.get(0, i);
-            _bounds_table.get(i, x) = bound_t(-1 * m, false) + _bounds_table.get(i, 0);
+            _bounds_table.get(i, x) = bound_t(-m, false) + _bounds_table.get(i, 0);
         }
     }
 
@@ -115,7 +115,7 @@ namespace dbm {
         for (int i = 0; i < this->_bounds_table._number_of_clocks; i++) {
             if (i != x) {
                 this->_bounds_table.get(x, i) = this->_bounds_table.at(x, i) + bound_t(n, false);
-                this->_bounds_table.get(i, x) = this->_bounds_table.at(i, x) + bound_t(-1 * n, false);
+                this->_bounds_table.get(i, x) = this->_bounds_table.at(i, x) + bound_t(-n, false);
             }
         }
         this->_bounds_table.get(x, 0) = bound_t::max(this->_bounds_table.at(x, 0), bound_t::zero());
@@ -133,7 +133,7 @@ namespace dbm {
                     this->_bounds_table.get(i, j) = bound_t::inf();
                 }
                 else if (!(this->_bounds_table.at(i, j)._inf) && this->_bounds_table.at(i, j) < bound_t(-ceiling[j], true)) {
-                    this->_bounds_table.get(i, j) = bound_t(-1 * ceiling[j], true);
+                    this->_bounds_table.get(i, j) = bound_t(-ceiling[j], true);
                 }
             }
         }
