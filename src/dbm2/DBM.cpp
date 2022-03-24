51f233c35cd72d3655e1d91edf881405b5e1c067
Thomas M.G <tgrose17@student.aau.dk>
2022-03-24 11:35:39 +0100

    fix isUnbounded

--- src/dbm2/DBM.cpp
+++ src/dbm2/DBM.cpp
@@ -72,7 +72,7 @@ namespace dbm2 {
 
     bool DBM::is_unbounded() const {
         for (dim_t i = 1; i < dimension(); ++i)
-            if (this->at(i, 0)._inf)
+            if (not this->at(i, 0).is_inf())
                 return false;
 
         return true;
