2989da678df2b75b01d21bc4b5ebe20608383da3
Thomas <thomasmgrosen@gmail.com>
2023-07-14 11:10:07 +0200

    temporarily remove exceptions from difference_bound_t.h

--- src/pardibaal/difference_bound_t.h
+++ src/pardibaal/difference_bound_t.h
@@ -23,7 +23,7 @@
 #define PARDIBAAL_DIFFERENCE_BOUND_T_H
 
 #include "bound_t.h"
-#include "errors.h"
+// #include "errors.h"
 
 namespace pardibaal {
 
@@ -37,34 +37,34 @@ namespace pardibaal {
         static constexpr difference_bound_t inf(dim_t i, dim_t j) {return difference_bound_t(i, j, bound_t::inf());}
 
         static constexpr difference_bound_t upper_strict(dim_t x, val_t n)  {
-#ifndef NEXCEPTIONS
-            if (n < 0)
-                throw(base_error("ERROR: Constructing constraint, clock bound should not be negative"));
-#endif
+// #ifndef NEXCEPTIONS
+//             if (n < 0)
+//                 throw(base_error("ERROR: Constructing constraint, clock bound should not be negative"));
+// #endif
             return difference_bound_t(x, 0, bound_t::strict(n));
         }
 
         static constexpr difference_bound_t upper_non_strict(dim_t x, val_t n) {
-#ifndef NEXCEPTIONS
-            if (n < 0)
-                throw(base_error("ERROR: Constructing constraint, clock bound should not be negative"));
-#endif
+// #ifndef NEXCEPTIONS
+//             if (n < 0)
+//                 throw(base_error("ERROR: Constructing constraint, clock bound should not be negative"));
+// #endif
             return difference_bound_t(x, 0, bound_t::non_strict(n));
         }
 
         static constexpr difference_bound_t lower_strict(dim_t x, val_t n) {
-#ifndef NEXCEPTIONS
-            if (n < 0)
-                throw(base_error("ERROR: Constructing constraint, clock bound should not be negative"));
-#endif
+// #ifndef NEXCEPTIONS
+//             if (n < 0)
+//                 throw(base_error("ERROR: Constructing constraint, clock bound should not be negative"));
+// #endif
             return difference_bound_t(0, x, bound_t::strict(-n));
         }
 
         static constexpr difference_bound_t lower_non_strict(dim_t x, val_t n) {
-#ifndef NEXCEPTIONS
-            if (n < 0)
-                throw(base_error("ERROR: Constructing constraint, clock bound should not be negative"));
-#endif
+// #ifndef NEXCEPTIONS
+//             if (n < 0)
+//                 throw(base_error("ERROR: Constructing constraint, clock bound should not be negative"));
+// #endif
             return difference_bound_t(0, x, bound_t::non_strict(-n));
         }
 
