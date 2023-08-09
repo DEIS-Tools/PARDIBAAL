6cc28e195d6c34a256dec118268edd5add8cf23a
Thomas <thomasmgrosen@gmail.com>
2023-08-09 13:09:27 +0200

    make bounds + more efficient

--- src/pardibaal/bound_t.cpp
+++ src/pardibaal/bound_t.cpp
@@ -40,7 +40,7 @@ namespace pardibaal {
         // If both are non-strict (first bit is 1), then the result should be non strict
         const val_t strictness = 1 & val1 & val2;
 
-        return bound_t((((val1 >> 1) + (val2 >> 1)) << 1) | strictness);
+        return bound_t(val1 + val2 - ((val1 & 1) | (val2 & 1)));
     }
 
     bound_t bound_t::operator+(val_t rhs) const {
